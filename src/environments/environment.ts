eea2b91fdc66e1cd59c1fb64a599cfc4b9f7f724
ismaelBM-best <ismaelbm432@gmail.com>
2026-04-22 05:17:47 -0500

    feat: initialize firebase and remote config in main bootstrap

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDkBRD3UQS1gtiQATCE_qUJqN1EtxcYSi4",
+    authDomain: "todoapp-ionic-84652.firebaseapp.com",
+    projectId: "todoapp-ionic-84652",
+    storageBucket: "todoapp-ionic-84652.firebasestorage.app",
+    messagingSenderId: "35984094733",
+    appId: "1:35984094733:web:2d3f5d61492c563cb00744"
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+// IMPORTS DE FIREBASE
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideRemoteConfig, getRemoteConfig } from '@angular/fire/remote-config';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideRouter(routes),
+    
+    // INICIALIZACIÓN DE FIREBASE
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideRemoteConfig(() => getRemoteConfig()),
   ],
 });
