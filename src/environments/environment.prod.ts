40baf6db7ffca006229a045d78bc8336f6083e41
ismaelBM-best <ismaelbm432@gmail.com>
2026-04-23 04:41:38 -0500

    feat: remote config and apk budgets solution problems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDkBRD3UQS1gtiQATCE_qUJqN1EtxcYSi4",
+    authDomain: "todoapp-ionic-84652.firebaseapp.com",
+    projectId: "todoapp-ionic-84652",
+    storageBucket: "todoapp-ionic-84652.firebasestorage.app",
+    messagingSenderId: "35984094733",
+    appId: "1:35984094733:web:2d3f5d61492c563cb00744"
+  }
 };
